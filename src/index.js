05c739ea828755719e1df9d49f26a59acc2d1c60
Sig Narváez <sigfrido.narvaez@10gen.com>
2023-02-27 20:56:17 -0800

    Issue 1
    GraphQL endpoint in readme and index.js

--- src/index.js
+++ src/index.js
@@ -47,7 +47,7 @@ async function getValidAccessToken() {
 // Configure the ApolloClient to connect to your app's GraphQL endpoint
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: `https://realm.mongodb.com/api/client/v2.0/app/${APP_ID}/graphql`,
+    uri: `<Your GraphQL Endpoint (URL)>`,
     // We define a custom fetch handler for the Apollo client that lets us authenticate GraphQL requests.
     // The function intercepts every Apollo HTTP request and adds an Authorization header with a valid
     // access token before sending the request.
